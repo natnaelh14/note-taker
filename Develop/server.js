63d02d31f588f9ec7baab0b260ec0714e1fbe415
Natnael Haile <natnaelhaile@Natnaels-MacBook-Pro.local>
2021-07-15 18:42:19 -0400

    created js files to establish the route

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const session = require('express-session')
+// const session = require('express-session')
+const path = require('path');
 
 // Sets up the Express App
 const app = express();
@@ -9,7 +10,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('./public'));
+// Basic route that sends the user first to the AJAX Page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/assets', 'index.html')));
+
+app.get('/notes/', (req, res) => res.sendFile(path.join(__dirname, 'public/assets', 'notes.html')));
+
 
 // Starts the server to begin listening
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
